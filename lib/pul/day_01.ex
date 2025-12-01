2130f93d2d242788e5bfe6f2ab6bac6e0e23cd70
Pul <pul@pulsit.de>
2025-12-01 17:29:10 +0100

    Solved day 01, part 01

--- lib/pul/day_01.ex
+++ lib/pul/day_01.ex
@@ -1,6 +1,18 @@
 defmodule Pul.Day01 do
-  def part1(args) do
-  end
+
+  def count_zero_passes(dials, dial_and_count \\ {50, 0})
+  def count_zero_passes(<<?R, h, d, s, ?\n, dials::binary>>, dc), do: count_zero_passes(dials, dial(100*(h-?0) + 10*(d-?0) + (s-?0), dc))
+  def count_zero_passes(<<?R, d, s, ?\n, dials::binary>>, dc), do: count_zero_passes(dials, dial(10*(d-?0) + (s-?0), dc))
+  def count_zero_passes(<<?R, s, ?\n, dials::binary>>, dc), do: count_zero_passes(dials, dial((s-?0), dc))
+  def count_zero_passes(<<?L, h, d, s, ?\n, dials::binary>>, dc), do: count_zero_passes(dials, dial(-100*(h-?0) - 10*(d-?0) - (s-?0), dc))
+  def count_zero_passes(<<?L, d, s, ?\n, dials::binary>>, dc), do: count_zero_passes(dials, dial(-10*(d-?0) - (s-?0), dc))
+  def count_zero_passes(<<?L, s, ?\n, dials::binary>>, dc), do: count_zero_passes(dials, dial(-(s-?0), dc))
+  def count_zero_passes("", {_dial, count}), do: count
+
+  defp dial(offset, {dial, _count} = dc) when dial + offset < 0, do: dial(offset + 100, dc)
+  defp dial(offset, {dial, _count} = dc) when dial + offset > 99, do: dial(offset - 100, dc)
+  defp dial(offset, {dial, count}) when dial == -offset, do: {0, count + 1}
+  defp dial(offset, {dial, count}), do: {dial + offset, count}
 
   def part2(args) do
   end
--- test/pul/day_01_test.exs
+++ test/pul/day_01_test.exs
@@ -3,8 +3,26 @@ defmodule Pul.Day01Test do
   # doctest Aoc2025
 
   alias Pul.Day01
+  @example """
+  L68
+  L30
+  R48
+  L5
+  R60
+  L55
+  L1
+  L99
+  R14
+  L82
+  """
+  @part01 File.read!("./test/pul/data/01_01")
 
-  describe "" do
-    test ""
+  describe "Day 1" do
+    test "part 1 example" do
+      assert Day01.count_zero_passes(@example) == 3
+    end
+    test "part 1 data" do
+      assert Day01.count_zero_passes(@part01) == 995
+    end
   end
 end
