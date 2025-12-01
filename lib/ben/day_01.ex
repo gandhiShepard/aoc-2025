166cf32bc1d7fbb676ff1accaf690c75f5a51bed
gandhiShepard <benjamin@gandhishepard.com>
2025-12-01 17:19:50 -0600

    refactors

--- lib/ben/day_01.ex
+++ lib/ben/day_01.ex
@@ -18,7 +18,7 @@ defmodule Ben.Day01 do
   L1
   L99
   R14
-  L82
+  L8
 
   ### Example usage
   Input must be a path to a data file in the priv directory.
@@ -34,27 +34,38 @@ defmodule Ben.Day01 do
   def part1("priv/" <> _rest_of_path = path_to_data) do
     path_to_data
     |> stream_input()
-    |> Enum.reduce([50], fn
-      "L" <> increment_string, [current_position | _rest] = acc ->
-        {increments, _} = Integer.parse(increment_string)
-        new_position = rem(current_position - increments + 100, 100)
-        [new_position | acc]
-
-      "R" <> increment_string, [current_position | _rest] = acc ->
-        {increments, _} = Integer.parse(increment_string)
-        new_position = rem(current_position + increments, 100)
-        [new_position | acc]
-    end)
-    |> Enum.count(&(&1 == 0))
+    |> Enum.reduce({50, 0}, &count_zero_positions/2)
+    |> elem(1)
   end
 
   def part1(_incorrect_input),
     do: raise(ArgumentError, "input must be a path to a file in the priv directory")
 
+  defp count_zero_positions(
+         <<direction>> <> increment_string = _instruction,
+         {current_position, total_times_pointing_at_zero} = _accumulator
+       ) do
+    # Parse instuction to get increments
+    {increments, _} = Integer.parse(increment_string)
+
+    new_position =
+      case <<direction>> do
+        "L" -> rem(current_position - increments + 100, 100)
+        "R" -> rem(current_position + increments, 100)
+      end
+
+    total_times_pointing_at_zero =
+      if new_position == 0,
+        do: 1 + total_times_pointing_at_zero,
+        else: total_times_pointing_at_zero
+
+    {new_position, total_times_pointing_at_zero}
+  end
+
   # ------------------------------------------------------------------------------
 
   @doc ~S"""
-  You remember from the training seminar that "method 0x434C49434B" means you're actually supposed to count the number of times any click causes the dial to point at 0, regardless of whether it happens during a rotation or at the end of one.
+  Now we need to count the total amount of times the dial points at 0 when executiong each instruction to get the password to the North Pole Base.
 
     iex> part2("priv/data/ben/day_01_example_data.txt")
     6
@@ -66,38 +77,42 @@ defmodule Ben.Day01 do
   def part2("priv/" <> _rest_of_path = path_to_data) do
     path_to_data
     |> stream_input()
-    |> Enum.reduce([{50, 0}], fn
-      "L" <> increment_string, [{current_position, _times_passing_zero} | _rest] = acc ->
-        {increments, _} = Integer.parse(increment_string)
-        times_passing_zero = div(increments, 100)
-        remaining_increments_to_rotate = rem(increments, 100)
-        new_position = rem(current_position - remaining_increments_to_rotate + 100, 100)
-
-        total_times_passing_zero =
-          if current_position != 0 && current_position - remaining_increments_to_rotate <= 0,
-            do: 1 + times_passing_zero,
-            else: times_passing_zero
-
-        [{new_position, total_times_passing_zero} | acc]
-
-      "R" <> increment_string, [{current_position, _rotations} | _rest] = acc ->
-        {increments, _} = Integer.parse(increment_string)
-        times_passing_zero = div(increments, 100)
-        remaining_increments_to_rotate = rem(increments, 100)
-        new_position = rem(current_position + remaining_increments_to_rotate, 100)
-
-        total_times_passing_zero =
-          if current_position != 0 && current_position + remaining_increments_to_rotate >= 100,
-            do: 1 + times_passing_zero,
-            else: times_passing_zero
-
-        [{new_position, total_times_passing_zero} | acc]
-    end)
-    |> Enum.reduce(0, fn {_, times_passing_zero}, acc -> acc + times_passing_zero end)
+    |> Enum.reduce({50, 0}, &count_zero_passes/2)
+    |> elem(1)
   end
 
   def part2(_incorrect_input),
     do: raise(ArgumentError, "input must be a path to a file in the priv directory")
+
+  defp count_zero_passes(
+         <<direction>> <> increment_string = _instruction,
+         {current_position, total_times_pointing_at_zero} = _accumulator
+       ) do
+    {increments, _} = Integer.parse(increment_string)
+    times_passing_zero = div(increments, 100)
+    remaining_increments_to_rotate = rem(increments, 100)
+
+    new_position =
+      case <<direction>> do
+        "L" -> rem(current_position - remaining_increments_to_rotate + 100, 100)
+        "R" -> rem(current_position + remaining_increments_to_rotate, 100)
+      end
+
+    additional_times_pointing_at_zero =
+      case <<direction>> do
+        "L" ->
+          if current_position != 0 && current_position - remaining_increments_to_rotate <= 0,
+            do: 1 + times_passing_zero,
+            else: times_passing_zero
+
+        "R" ->
+          if current_position != 0 && current_position + remaining_increments_to_rotate >= 100,
+            do: 1 + times_passing_zero,
+            else: times_passing_zero
+      end
+
+    {new_position, total_times_pointing_at_zero + additional_times_pointing_at_zero}
+  end
 
   # ------------------------------------------------------------------------------
 
--- priv/perf/day_01_perf.exs
+++ priv/perf/day_01_perf.exs
@@ -18,3 +18,19 @@ Benchee.run(
   },
   inputs: test_data
 )
+
+# ------------------------------------------------------------------------------
+
+# First Solutions
+
+##### With input bens_data_stream #####
+# Name                 ips        average  deviation         median         99th %
+# BEN_PART_1        1.05 K        0.95 ms     ±4.09%        0.94 ms        1.06 ms
+# BEN_PART_2        0.97 K        1.03 ms     ±9.01%        1.04 ms        1.19 ms
+
+# Refactored Solutions
+##### With input bens_data_stream #####
+# Name                 ips        average  deviation         median         99th %
+# BEN_PART_1        1.17 K      855.57 μs     ±5.02%      850.96 μs      982.18 μs
+# BEN_PART_2        1.13 K      887.28 μs    ±15.85%         883 μs     1011.86 μs
+
