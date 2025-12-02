d9876ed124fd5d9aea2e6e50939580555e9b74cf
Pul <pul@pulsit.de>
2025-12-02 16:40:39 +0100

    Downswitch to Elixir 1.16

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Aoc2025.MixProject do
     [
       app: :aoc_2025,
       version: "0.1.0",
-      elixir: "~> 1.19",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
