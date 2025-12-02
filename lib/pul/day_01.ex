e25e03d6f44d0dc78b814f68ce3abf330f916f94
Pul <pul@pulsit.de>
2025-12-02 16:38:42 +0100

    Day 1 completed and refactored

--- lib/pul/day_01.ex
+++ lib/pul/day_01.ex
@@ -1,19 +1,49 @@
 defmodule Pul.Day01 do
+  @initial_dial_position 50
 
-  def count_zero_passes(dials, dial_and_count \\ {50, 0})
-  def count_zero_passes(<<?R, h, d, s, ?\n, dials::binary>>, dc), do: count_zero_passes(dials, dial(100*(h-?0) + 10*(d-?0) + (s-?0), dc))
-  def count_zero_passes(<<?R, d, s, ?\n, dials::binary>>, dc), do: count_zero_passes(dials, dial(10*(d-?0) + (s-?0), dc))
-  def count_zero_passes(<<?R, s, ?\n, dials::binary>>, dc), do: count_zero_passes(dials, dial((s-?0), dc))
-  def count_zero_passes(<<?L, h, d, s, ?\n, dials::binary>>, dc), do: count_zero_passes(dials, dial(-100*(h-?0) - 10*(d-?0) - (s-?0), dc))
-  def count_zero_passes(<<?L, d, s, ?\n, dials::binary>>, dc), do: count_zero_passes(dials, dial(-10*(d-?0) - (s-?0), dc))
-  def count_zero_passes(<<?L, s, ?\n, dials::binary>>, dc), do: count_zero_passes(dials, dial(-(s-?0), dc))
-  def count_zero_passes("", {_dial, count}), do: count
+  def dialed_zeroes(rotations),
+    do: reduce_rotations(rotations, 0, &dialed_zeroes/2)
 
-  defp dial(offset, {dial, _count} = dc) when dial + offset < 0, do: dial(offset + 100, dc)
-  defp dial(offset, {dial, _count} = dc) when dial + offset > 99, do: dial(offset - 100, dc)
-  defp dial(offset, {dial, count}) when dial == -offset, do: {0, count + 1}
-  defp dial(offset, {dial, count}), do: {dial + offset, count}
-
-  def part2(args) do
+  defp dialed_zeroes(offset, {position, zeroes}) do
+    case dial(position, offset) do
+      0 -> {0, zeroes + 1}
+      position when position in 1..99 -> {position, zeroes}
+    end
   end
+
+  defp dial(position, offset) when position + offset < 0, do: dial(position, offset + 100)
+  defp dial(position, offset) when position + offset > 99, do: dial(position, offset - 100)
+  defp dial(position, offset), do: position + offset
+
+  def passed_zeroes(rotations),
+    do: reduce_rotations(rotations, 0, &passed_zeroes/2)
+
+  defp passed_zeroes(offset, {position, zeroes}) do
+    zeroes = zeroes + zeroes_between(position, position + offset)
+    case dial(position, offset) do
+      0 -> {0, zeroes + 1}
+      position when position in 1..99 -> {position, zeroes}
+    end
+  end
+
+  defp zeroes_between(pos, o_pos) when pos in 0..99 and o_pos > 99 and rem(o_pos, 100) == 0, do: div(o_pos, 100) - 1
+  defp zeroes_between(pos, o_pos) when pos in 0..99 and o_pos > 99, do: div(o_pos, 100)
+  defp zeroes_between(pos, o_pos) when pos in 1..99 and o_pos < 0 and rem(o_pos, 100) == 0, do: -div(o_pos, 100) + 1 - 1
+  defp zeroes_between(pos, o_pos) when pos in 1..99 and o_pos < 0, do: -div(o_pos, 100) + 1
+  defp zeroes_between(0, o_pos) when o_pos < 0 and rem(o_pos, 100) == 0, do: -div(o_pos, 100) - 1
+  defp zeroes_between(0, o_pos) when o_pos < 0, do: -div(o_pos, 100)
+  defp zeroes_between(_, _), do: 0
+
+  defp reduce_rotations(rotations, initial, reducer) when is_function(reducer, 2),
+    do:
+      rotations
+      |> Stream.map(&offset/1)
+      |> Stream.reject(&is_nil/1)
+      |> Enum.reduce({@initial_dial_position, initial}, reducer)
+      |> then(fn {_final_position, total} -> total end)
+
+  defp offset("L" <> clicks), do: - String.to_integer(clicks)
+  defp offset("R" <> clicks), do: String.to_integer(clicks)
+  defp offset(_), do: nil
+
 end
--- test/pul/day_01_test.exs
+++ test/pul/day_01_test.exs
@@ -1,28 +1,24 @@
 defmodule Pul.Day01Test do
   use ExUnit.Case, async: true
-  # doctest Aoc2025
-
   alias Pul.Day01
-  @example """
-  L68
-  L30
-  R48
-  L5
-  R60
-  L55
-  L1
-  L99
-  R14
-  L82
-  """
-  @part01 File.read!("./test/pul/data/01_01")
 
-  describe "Day 1" do
-    test "part 1 example" do
-      assert Day01.count_zero_passes(@example) == 3
+  defp testsample, do: File.stream!("./test/pul/data/01_01_unit") |> Stream.map(&String.trim/1)
+  defp testdata, do: File.stream!("./test/pul/data/01_01") |> Stream.map(&String.trim/1)
+
+  describe "Day 01, part 01" do
+    test "works with example" do
+      assert Day01.dialed_zeroes(testsample()) == 3
     end
-    test "part 1 data" do
-      assert Day01.count_zero_passes(@part01) == 995
+    test "works with test data" do
+      assert Day01.dialed_zeroes(testdata()) == 995
+    end
+  end
+  describe "Day 01, part 02" do
+    test "works with example" do
+      assert Day01.passed_zeroes(testsample()) == 6
+    end
+    test "works with test data" do
+      assert Day01.passed_zeroes(testdata()) == 5847
     end
   end
 end
