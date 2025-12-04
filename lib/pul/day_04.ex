5afbc7a8b3ffdad72b40b91a86fbdc59c6ffc40e
Pul <pul@pulsit.de>
2025-12-04 08:15:32 +0100

    Day 04 finished - MapSet instead Map, - better naming - more consistent code structure

--- lib/pul/day_04.ex
+++ lib/pul/day_04.ex
@@ -1,42 +1,37 @@
 defmodule Pul.Day04 do
 
-  @roll ?@
-  def count_of_accessible_paper_rolls(grid_rows),
+  def count_of_accessible_paper_rolls(grid_rows = %Stream{}),
     do:
-      grid_rows
-      |> to_map()
-      |> then(fn grid_map -> Enum.count(grid_map, &accessible_paper_roll?(&1, grid_map)) end)
+      with(
+        paper_rolls = paper_rolls(grid_rows),
+        do: Enum.count(paper_rolls, &accessible?(&1, paper_rolls)))
 
-  defp to_map(grid_rows = %Stream{}),
+  defp paper_rolls(grid_rows = %Stream{}),
     do:
       grid_rows
       |> Stream.with_index(0)
-      |> Enum.reduce(%{}, fn {row, r}, map when is_binary(row) and is_integer(r)->
-        for({@roll, c} <- to_charlist(row) |> Enum.with_index(0), into: map, do: {{r, c}, @roll})
+      |> Enum.reduce(MapSet.new(), fn {row, r}, paper_rolls when is_binary(row) and is_integer(r)->
+        for({?@, c} <- to_charlist(row) |> Enum.with_index(0), into: paper_rolls, do: {r, c})
       end)
 
-  defp accessible_paper_roll?({{r, c}, @roll}, grid_map = %{}),
+  defp accessible?({r, c}, paper_rolls = %MapSet{}),
     do:
-      (for dr <- -1..1,
-        dc <- -1..1,
-        not(dr == 0 and dc == 0),
-        Map.get(grid_map, {r + dr, c + dc}) == @roll,
-        reduce: 0 do
-          neighbour_rolls -> neighbour_rolls + 1
-        end) < 4
+      (for dr <- -1..1, dc <- -1..1, not(dr == 0 and dc == 0), MapSet.member?(paper_rolls, {r + dr, c + dc}), reduce: 0 do
+        neighbour_rolls -> neighbour_rolls + 1
+      end) < 4
 
-  def count_of_removable_paper_rolls(grid_rows),
+  def count_of_removable_paper_rolls(grid_rows = %Stream{}),
     do:
       with(
-        original_map = to_map(grid_rows),
-        map_after_removing = remove_all_accessible_paper_rolls(original_map),
-        do: Enum.count(original_map) - Enum.count(map_after_removing))
+        paper_rolls = paper_rolls(grid_rows),
+        remaining_paper_rolls = remove_all_accessible_paper_rolls(paper_rolls),
+        do: Enum.count(paper_rolls) - Enum.count(remaining_paper_rolls))
 
-  defp remove_all_accessible_paper_rolls(grid_map = %{}) do
-    case (for cell <- grid_map, !accessible_paper_roll?(cell, grid_map), into: %{}, do: cell) do
-      ^grid_map -> grid_map
-      map_after_removing -> remove_all_accessible_paper_rolls(map_after_removing)
-    end
-  end
+  defp remove_all_accessible_paper_rolls(paper_rolls = %MapSet{}),
+    do:
+      (case (for paper_roll <- paper_rolls, !accessible?(paper_roll, paper_rolls), into: MapSet.new(), do: paper_roll) do
+        ^paper_rolls -> paper_rolls
+        remaining_paper_rolls -> remove_all_accessible_paper_rolls(remaining_paper_rolls)
+      end)
 
 end
