6e1df3cc018a80afeb9ba08e6a7996e6f47e08d5
Pul <pul@pulsit.de>
2025-12-04 07:31:39 +0100

    Day 04 part 1 finished

--- lib/pul/day_04.ex
+++ lib/pul/day_04.ex
@@ -1,5 +1,29 @@
 defmodule Pul.Day04 do
-  def count_of_accesible_paper_rolls(grid_rows),
-    do: grid_rows
+
+  @roll ?@
+  def count_of_accessible_paper_rolls(grid_rows),
+    do:
+      grid_rows
+      |> to_map()
+      |> then(fn grid_map -> Enum.count(grid_map, &accessible_paper_roll?(&1, grid_map)) end)
+
+  defp to_map(grid_rows = %Stream{}),
+    do:
+      grid_rows
+      |> Stream.with_index(0)
+      |> Enum.reduce(%{}, fn {row, r}, map when is_binary(row) and is_integer(r)->
+        for({@roll, c} <- to_charlist(row) |> Enum.with_index(0), into: map, do: {{r, c}, @roll})
+      end)
+
+  defp accessible_paper_roll?({{r, c}, @roll}, grid_map = %{}),
+    do:
+      (for dr <- -1..1,
+        dc <- -1..1,
+        not(dr == 0 and dc == 0),
+        Map.get(grid_map, {r + dr, c + dc}) == @roll,
+        reduce: 0 do
+          neighbour_rolls -> neighbour_rolls + 1
+        end) < 4
+
 
 end
--- test/pul/day_04_test.exs
+++ test/pul/day_04_test.exs
@@ -8,18 +8,18 @@ defmodule Pul.Day04Test do
 
   describe "Day 04, part 01" do
     test "works with example" do
-      assert Day04.count_of_accesible_paper_rolls(testsample()) == 13
+      assert Day04.count_of_accessible_paper_rolls(testsample()) == 13
     end
     test "works with test data" do
-      assert Day04.count_of_accesible_paper_rolls(testdata()) == nil
+      assert Day04.count_of_accessible_paper_rolls(testdata()) == 1491
     end
   end
   describe "Day 04, part 02" do
     test "works with example" do
-      assert Day04.count_of_accesible_paper_rolls(testsample()) == nil
+      assert Day04.count_of_accessible_paper_rolls(testsample()) == nil
     end
     test "works with test data" do
-      assert Day04.count_of_accesible_paper_rolls(testdata()) == nil
+      assert Day04.count_of_accessible_paper_rolls(testdata()) == nil
     end
   end
 end
