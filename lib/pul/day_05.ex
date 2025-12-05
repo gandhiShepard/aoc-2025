125f153415fad68590f92f0a8046f7b78d4852e8
Pul <pul@pulsit.de>
2025-12-05 07:49:28 +0100

    Prepared day 05

---
+++ lib/pul/day_05.ex
@@ -0,0 +1,5 @@
+defmodule Pul.Day05 do
+
+  def count_of_fresh_ingredients(inventory_list = %Stream{}),
+    do: inventory_list
+end
---
+++ test/pul/day_05_test.exs
@@ -0,0 +1,25 @@
+defmodule Pul.Day05Test do
+  use ExUnit.Case, async: true
+  alias Pul.Day05
+  doctest Day05, import: true
+
+  defp testsample, do: File.stream!("./test/pul/data/05_01_unit") |> Stream.map(&String.trim/1)
+  defp testdata, do: File.stream!("./test/pul/data/05_01") |> Stream.map(&String.trim/1)
+
+  describe "Day 05, part 01" do
+    test "works with example" do
+      assert Day05.count_of_fresh_ingredients(testsample()) == 3
+    end
+    test "works with test data" do
+      assert Day05.count_of_fresh_ingredients(testdata()) == nil
+    end
+  end
+  # describe "Day 05, part 02" do
+  #   test "works with example" do
+  #     assert Day05.count_of_removable_paper_rolls(testsample()) == nil
+  #   end
+  #   test "works with test data" do
+  #     assert Day05.count_of_removable_paper_rolls(testdata()) == 8722
+  #   end
+  # end
+end
