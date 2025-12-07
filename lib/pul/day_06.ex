68570c2c689b46ea02b9634e93b74c347e060910
Pul <pul@pulsit.de>
2025-12-07 11:19:01 +0100

    Day 06, part 02 finished

--- lib/pul/day_06.ex
+++ lib/pul/day_06.ex
@@ -1,5 +1,7 @@
 defmodule Pul.Day06 do
 
+  defguardp is_op(op) when op in [?+, ?*]
+
   def grand_total_of_solved_tasks(task_table = %Stream{}),
     do:
       with(
@@ -25,5 +27,43 @@ defmodule Pul.Day06 do
 
   defp reducer("+"), do: &Kernel.+/2
   defp reducer("*"), do: &Kernel.*/2
+  defp reducer(?+), do: &Kernel.+/2
+  defp reducer(?*), do: &Kernel.*/2
+
+  def grand_total_of_solved_tasks(task_table = %Stream{}, :right_to_left),
+    do:
+      with(
+        [operator_line | reversed_parameter_lines] = Enum.reduce(task_table, [], &[&1 | &2]),
+        do: total(operator_line, Enum.reverse(reversed_parameter_lines), 0))
+
+  defp total(<<op::8, ?\s::8, ?\s::8, ?\s::8, ?\n::8>>, parameter_lines, total)
+    when is_op(op), do: evaluate(op, 4, <<>>, parameter_lines, total)
+  defp total(<<op::8, ?\s::8, ?\s::8, ?\n::8>>, parameter_lines, total)
+    when is_op(op), do: evaluate(op, 3, <<>>, parameter_lines, total)
+  defp total(<<op::8, ?\s::8, ?\n::8>>, parameter_lines, total)
+    when is_op(op), do: evaluate(op, 2, <<>>, parameter_lines, total)
+  defp total(<<op::8>>, parameter_lines, total)
+    when is_op(op), do: evaluate(op, 1, <<>>, parameter_lines, total)
+  defp total(<<op::8, ?\s::8, ?\s::8, ?\s::8, ?\s::8, ?\s::8, operator_line::binary>>, parameter_lines, total)
+    when is_op(op), do: evaluate(op, 5, operator_line, parameter_lines, total)
+  defp total(<<op::8, ?\s::8, ?\s::8, ?\s::8, ?\s::8, operator_line::binary>>, parameter_lines, total)
+    when is_op(op), do: evaluate(op, 4, operator_line, parameter_lines, total)
+  defp total(<<op::8, ?\s::8, ?\s::8, ?\s::8, operator_line::binary>>, parameter_lines, total)
+    when is_op(op), do: evaluate(op, 3, operator_line, parameter_lines, total)
+  defp total(<<op::8, ?\s::8, ?\s::8, operator_line::binary>>, parameter_lines, total)
+    when is_op(op), do: evaluate(op, 2, operator_line, parameter_lines, total)
+  defp total(<<op::8, ?\s::8, operator_line::binary>>, parameter_lines, total)
+    when is_op(op), do: evaluate(op, 1, operator_line, parameter_lines, total)
+  defp total(<<>>, _, total), do: total
+
+  defp evaluate(op, n_params, operator_line, parameter_lines, total) when is_op(op) and n_params in 1..4,
+    do:
+      with(
+        [p1 | parameters] = for(p <- 1..n_params, do:
+          for(l <- parameter_lines, digit = String.at(l, p - 1), digit != " ", do: digit)
+          |> Enum.join("") |> String.to_integer()),
+        total = total + Enum.reduce(parameters, p1, reducer(op)),
+        parameter_lines = Enum.map(parameter_lines, &String.slice(&1, n_params + 1, 100_000)),
+        do: total(operator_line, parameter_lines, total))
 
 end
--- test/pul/day_06_test.exs
+++ test/pul/day_06_test.exs
@@ -3,8 +3,8 @@ defmodule Pul.Day06Test do
   alias Pul.Day06
   doctest Day06, import: true
 
-  defp testsample, do: File.stream!("./test/pul/data/06_01_unit") |> Stream.map(&String.trim/1)
-  defp testdata, do: File.stream!("./test/pul/data/06_01") |> Stream.map(&String.trim/1)
+  defp testsample, do: File.stream!("./test/pul/data/06_01_unit") |> Stream.map(& &1) # don't trim!
+  defp testdata, do: File.stream!("./test/pul/data/06_01") |> Stream.map(& &1)
 
   describe "Day 06, part 01" do
     test "works with example" do
@@ -14,12 +14,12 @@ defmodule Pul.Day06Test do
       assert Day06.grand_total_of_solved_tasks(testdata()) == 5171061464548
     end
   end
-  # describe "Day 06, part 02" do
-  #   test "works with example" do
-  #     assert Day06.count_of_fresh_ingredients(testsample()) == 14
-  #   end
-  #   test "works with test data" do
-  #     assert Day06.count_of_fresh_ingredients(testdata()) == 344423158480189
-  #   end
-  # end
+  describe "Day 06, part 02" do
+    test "works with example" do
+      assert Day06.grand_total_of_solved_tasks(testsample(), :right_to_left) == 3263827
+    end
+    test "works with test data" do
+      assert Day06.grand_total_of_solved_tasks(testdata(), :right_to_left) == 10189959087258
+    end
+  end
 end
