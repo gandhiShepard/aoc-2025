a8cc9b3a96b68efc141ad324538077668a18d9a0
Pul <pul@pulsit.de>
2025-12-08 11:28:56 +0100

    Prepared day 08

---
+++ lib/pul/day_08.ex
@@ -0,0 +1,6 @@
+defmodule Pul.Day08 do
+
+  def multiply_three_largest_circuits(junction_box_positions = %Stream{}),
+    do:
+      junction_box_positions
+end
---
+++ test/pul/day_08_test.exs
@@ -0,0 +1,25 @@
+defmodule Pul.Day08Test do
+  use ExUnit.Case, async: true
+  alias Pul.Day08
+  doctest Day08, import: true
+
+  defp testsample, do: File.stream!("./test/pul/data/08_01_unit") |> Stream.map(&String.trim/1)
+  defp testdata, do: File.stream!("./test/pul/data/08_01") |> Stream.map(&String.trim/1)
+
+  describe "Day 08, part 01" do
+    test "works with example" do
+      assert Day08.multiply_three_largest_circuits(testsample()) == 40
+    end
+    test "works with test data" do
+      assert Day08.multiply_three_largest_circuits(testdata()) == nil
+    end
+  end
+  # describe "Day 08, part 02" do
+  #   test "works with example" do
+  #     assert Day08.count_of_timelines(testsample()) == 40
+  #   end
+  #   test "works with test data" do
+  #     assert Day08.count_of_timelines(testdata()) == 3223365367809
+  #   end
+  # end
+end
